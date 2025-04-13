77c3db2dffc5093fcd206c78be53fa73751437db
Posccis <lkzq33@gmail.com>
2025-04-13 19:20:13 -0300

    new(main): Cart entity creation

---
+++ template/backend/src/Ambev.DeveloperEvaluation.Domain/Entities/Cart.cs
@@ -0,0 +1,21 @@
+﻿using Ambev.DeveloperEvaluation.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ambev.DeveloperEvaluation.Domain.Entities
+{
+    public class Cart : ICart
+    {
+        public Guid Id { get;  set; }                     
+        public Guid CustomerId { get;  set; }             
+        public List<CartItem> Items { get;  set; }        
+        public decimal Subtotal => Items.Sum(i => i.TotalPrice); 
+        public decimal Total => Subtotal;             
+        public DateTime CreatedOn { get;  set; }
+        public DateTime UpdatedOn { get;  set; }
+        public bool IsActive { get;  set; }
+    }
+}
---
+++ template/backend/src/Ambev.DeveloperEvaluation.Domain/Interfaces/ICart.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ambev.DeveloperEvaluation.Domain.Interfaces
+{
+    public interface ICart
+    {
+        public Guid Id { get; set; }
+        public Guid CustomerId { get; set; }
+        public List<ICartItem> Items { get; set; }
+        public decimal Subtotal => Items.Sum(i => i.TotalPrice);
+        public decimal Total => Subtotal;
+        public DateTime CreatedOn { get; set; }
+        public DateTime UpdatedOn { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
