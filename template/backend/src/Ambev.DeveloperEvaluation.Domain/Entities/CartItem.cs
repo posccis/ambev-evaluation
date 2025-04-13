362229bec6070e838ef469abcd30345b6adad3a8
Posccis <lkzq33@gmail.com>
2025-04-13 19:21:05 -0300

    new(main): CartItem entity creation

---
+++ template/backend/src/Ambev.DeveloperEvaluation.Domain/Entities/CartItem.cs
@@ -0,0 +1,20 @@
+﻿using Ambev.DeveloperEvaluation.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ambev.DeveloperEvaluation.Domain.Entities
+{
+    public class CartItem : ICartItem
+    {
+        public Guid ProductId { get; set; }
+        public Guid CartId { get; set; }
+        public string ProductName { get; set; }
+        public decimal UnitPrice { get; set; }
+        public int Quantity { get; set; }
+        public decimal Total => UnitPrice * Quantity;
+
+    }
+}
