86e54c616b8efad117847dee587f0ea665d2ce3e
Posccis <lkzq33@gmail.com>
2025-04-13 19:25:42 -0300

    New(main): Order status creation

---
+++ template/backend/src/Ambev.DeveloperEvaluation.Domain/Enums/OrderStatus.cs
@@ -0,0 +1,17 @@
+﻿namespace Ambev.DeveloperEvaluation.Domain.Enums
+{
+    public enum OrderStatus
+    {
+        Created,        
+        AwaitingPayment,
+        Paid,           
+        Processing,     
+        Shipped,        
+        Delivered,      
+        Completed,      
+        Cancelled,      
+        Refunded,       
+        Failed          
+
+    }
+}
