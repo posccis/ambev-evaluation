ee0ff2b7fc51e6c481d8bab0d5dbc2f129219347
Posccis <lkzq33@gmail.com>
2025-04-13 19:23:49 -0300

    New(main): OrderItem entity creation

---
+++ template/backend/src/Ambev.DeveloperEvaluation.Domain/Entities/OrderItem.cs
@@ -0,0 +1,17 @@
+﻿using Ambev.DeveloperEvaluation.Domain.Common;
+using Ambev.DeveloperEvaluation.Domain.Interfaces;
+
+namespace Ambev.DeveloperEvaluation.Domain.Entities
+{
+    public class OrderItem : BaseEntity, IOrderItem
+    {
+        public Guid OrderId { get; set; }
+        public Guid ProductId { get; set; }
+        public string ProductName { get; set; }
+        public string? ProductSku { get; set; }
+        public decimal UnitPrice { get; set; }
+        public int Quantity { get; set; }
+        public DateTime CreatedOn => DateTime.Now;
+        public DateTime UpdatedOn { get; set; }
+    }
+}
