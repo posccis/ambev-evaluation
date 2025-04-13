b408ac5c92effc562842c29832a403be3217ec91
Posccis <lkzq33@gmail.com>
2025-04-13 19:26:20 -0300

    New(main): Order validator creation

---
+++ template/backend/src/Ambev.DeveloperEvaluation.Domain/Validation/OrderValidator.cs
@@ -0,0 +1,30 @@
+﻿using Ambev.DeveloperEvaluation.Domain.Entities;
+using Ambev.DeveloperEvaluation.Domain.Enums;
+using FluentValidation;
+
+namespace Ambev.DeveloperEvaluation.Domain.Validation;
+
+public class OrderValidator : AbstractValidator<Order>
+{
+    public OrderValidator()
+    {
+        RuleFor(od => od.CustomerId)
+            .NotEmpty()
+            .WithMessage("The customer ID must be informed.");
+
+        RuleFor(od => od.ShippingAddress)
+        .NotNull()
+        .WithMessage("The shipping address is cannot be null.");
+
+        RuleFor(od => od.Items)
+        .NotNull()
+        .NotEmpty()
+        .WithMessage("The order needs at least one item.");
+
+        RuleFor(od => od.PaymentMethod)
+        .NotNull()
+        .WithMessage("The order needs a valid payment method.");
+
+        
+    }
+}
