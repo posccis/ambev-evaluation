10f9e6f28b2121bbfcf9baee6ce341f3ea79a7b0
Posccis <lkzq33@gmail.com>
2025-04-22 01:14:27 -0300

    New(main): Criação do evento de pagamento

---
+++ template/backend/src/Ambev.DeveloperEvaluation.Domain/Events/ExecutePaymentEvent.cs
@@ -0,0 +1,19 @@
+﻿using Ambev.DeveloperEvaluation.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ambev.DeveloperEvaluation.Domain.Events
+{
+    public class ExecutePaymentEvent
+    {
+        public PaymentMethodBase PaymentMethod { get; set; }
+
+        public ExecutePaymentEvent(PaymentMethodBase paymentMethod)
+        {
+            PaymentMethod = paymentMethod;
+        }
+    }
+}
