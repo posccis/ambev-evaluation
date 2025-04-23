96d9eb2ca02441633f231fd4a2cbb1394d4e9aa0
Posccis <lkzq33@gmail.com>
2025-04-22 23:19:21 -0300

    New(main)= Desenvolvimento do UnitOfWork

---
+++ template/backend/src/Ambev.DeveloperEvaluation.ORM/Common/UnitOfWork.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ambev.DeveloperEvaluation.ORM.Common
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly DefaultContext _context;
+        public UnitOfWork(DefaultContext context)
+        {
+            _context = context;
+        }
+        public async Task<int> CommitAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
+    }
+}
