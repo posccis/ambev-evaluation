c02599ddd7b8984f42f69e2b63cc695a0f4020eb
Posccis <lkzq33@gmail.com>
2025-04-26 23:52:39 -0300

    New(main): Para passagem

--- template/backend/src/Ambev.DeveloperEvaluation.Domain/Events/ExecutePaymentEvent.cs
+++ template/backend/src/Ambev.DeveloperEvaluation.Domain/Events/ExecutePaymentEvent.cs
@@ -10,10 +10,12 @@ namespace Ambev.DeveloperEvaluation.Domain.Events
     public class ExecutePaymentEvent
     {
         public PaymentMethodBase PaymentMethod { get; set; }
+        public Guid OrderId { get; set; }
 
-        public ExecutePaymentEvent(PaymentMethodBase paymentMethod)
+        public ExecutePaymentEvent(PaymentMethodBase paymentMethod, Guid orderId)
         {
             PaymentMethod = paymentMethod;
+            OrderId = orderId;
         }
     }
 }
