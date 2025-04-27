c729453010a91a7fd01be5d8c63d6bea983ad189
Posccis <lkzq33@gmail.com>
2025-04-26 23:48:35 -0300

    New(main): desenvolvimento de biblioteca para lidar com as configurações do MongoDb

---
+++ template/Ambev.DeveloperEvaluation.NoSql/Context/NoSqlContext.cs
@@ -0,0 +1,30 @@
+﻿using Ambev.DeveloperEvaluation.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using MongoDB.Driver;
+using MongoDB.EntityFrameworkCore.Extensions;
+
+namespace Ambev.DeveloperEvaluation.NoSql.Context
+{
+    public class NoSqlContext : DbContext
+    {
+
+        public DbSet<Notification> Notifications { get; init; }
+
+        public static NoSqlContext Create(IMongoDatabase database) =>
+            new(new DbContextOptionsBuilder<NoSqlContext>()
+        .UseMongoDB(database.Client, database.DatabaseNamespace.DatabaseName)
+        .Options);
+
+        public NoSqlContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Notification>().ToCollection("Notifications");
+            modelBuilder.Entity<Notification>().HasKey(n => n.Id);
+        }
+
+    }
+}
