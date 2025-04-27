74fc283abb8c98afc9e8cf7e8628c7a922dbf956
Posccis <lkzq33@gmail.com>
2025-04-26 23:50:39 -0300

    New(main):Criação da entidade Notification para receber as notificações no Mongo

---
+++ template/backend/src/Ambev.DeveloperEvaluation.Domain/Entities/Notification.cs
@@ -0,0 +1,15 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+
+namespace Ambev.DeveloperEvaluation.Domain.Entities
+{
+    public class Notification
+    {
+        [BsonId]
+        public Guid Id { get; set; }
+        [BsonElement("body")]
+        public string Body { get; set; }
+        [BsonElement("orderid")]
+        public Guid OrderId { get; set; }
+    }
+}
