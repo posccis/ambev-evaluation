8c0612075197e2d03468c97e3a59a6f67e8a9912
Posccis <lkzq33@gmail.com>
2025-04-26 23:52:07 -0300

    Update(main): Adição de endpoint para retornar orders

--- template/backend/src/Ambev.DeveloperEvaluation.WebApi/Features/Shopping/ShoppingController.cs
+++ template/backend/src/Ambev.DeveloperEvaluation.WebApi/Features/Shopping/ShoppingController.cs
@@ -1,12 +1,14 @@
 ﻿using Ambev.DeveloperEvaluation.Application.Carts.AddItemToCart;
 using Ambev.DeveloperEvaluation.Application.Carts.CreateCart;
 using Ambev.DeveloperEvaluation.Application.Shopping.CreateOrder;
+using Ambev.DeveloperEvaluation.Application.Shopping.GetOrder;
 using Ambev.DeveloperEvaluation.Application.Users.CreateUser;
 using Ambev.DeveloperEvaluation.WebApi.Common;
 using Ambev.DeveloperEvaluation.WebApi.Features.Cart.AddItemToCart;
 using Ambev.DeveloperEvaluation.WebApi.Features.Cart.CreateCart;
 using Ambev.DeveloperEvaluation.WebApi.Features.Order.CreateOrder;
 using Ambev.DeveloperEvaluation.WebApi.Features.Shopping.CreateOrder;
+using Ambev.DeveloperEvaluation.WebApi.Features.Shopping.GetOrder;
 using Ambev.DeveloperEvaluation.WebApi.Features.Users.CreateUser;
 using AutoMapper;
 using MediatR;
@@ -61,6 +63,27 @@ namespace Ambev.DeveloperEvaluation.WebApi.Features.Shopping
                 Success = true,
                 Message = "Order created successfully",
                 Data = _mapper.Map<CreateOrderResponse>(response)
+            });
+        }
+
+        /// <summary>
+        /// Returns all orders in the database
+        /// </summary>
+        /// <param name="cancellationToken">Cancellation token</param>
+        /// <returns>The list of all orders</returns>
+        [HttpGet]
+        [ProducesResponseType(typeof(ApiResponseWithData<List<GetOrderResponse>>), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> Get(CancellationToken cancellationToken)
+        {
+            var query = new GetOrderQuery();
+            var response = await _mediator.Send(query, cancellationToken);
+
+            return Created(string.Empty, new ApiResponseWithData<GetOrderResponse>
+            {
+                Success = true,
+                Message = "Order created successfully",
+                Data = _mapper.Map<GetOrderResponse>(response)
             });
         }
 
